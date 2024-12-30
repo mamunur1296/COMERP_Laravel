320365f1a52e5f71468094c1840a81dcaf306171
Mamunur Rushid <mamunurrushid60@gmail.com>
2024-12-30 10:35:48 +0600

    Change Name Admin to adminsection

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -10,24 +10,24 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="{{ asset('admin/assets/img/favicon.png') }}" rel="icon">
-  <link href="{{ asset('admin/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+  <link href="{{ asset('adminsection/assets/img/favicon.png') }}" rel="icon">
+  <link href="{{ asset('adminsection/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="{{ asset('admin/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('admin/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-  <link href="{{ asset('admin/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('admin/assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
-  <link href="{{ asset('admin/assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
-  <link href="{{ asset('admin/assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
-  <link href="{{ asset('admin/assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+  <link href="{{ asset('adminsection/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('adminsection/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+  <link href="{{ asset('adminsection/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('adminsection/assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
+  <link href="{{ asset('adminsection/assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
+  <link href="{{ asset('adminsection/assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+  <link href="{{ asset('adminsection/assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="{{ asset('admin/assets/css/style.css') }}" rel="stylesheet">
+  <link href="{{ asset('adminsection/assets/css/style.css') }}" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: ComErp
@@ -143,7 +143,7 @@
 
             <li class="message-item">
               <a href="#">
-                <img src="{{asset('admin/assets/img/messages-1.jpg')}}" alt="" class="rounded-circle">
+                <img src="{{asset('adminsection/assets/img/messages-1.jpg')}}" alt="" class="rounded-circle">
                 <div>
                   <h4>Maria Hudson</h4>
                   <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
@@ -157,7 +157,7 @@
 
             <li class="message-item">
               <a href="#">
-                <img src="{{asset('admin/assets/img/messages-2.jpg')}}" alt="" class="rounded-circle">
+                <img src="{{asset('adminsection/assets/img/messages-2.jpg')}}" alt="" class="rounded-circle">
                 <div>
                   <h4>Anna Nelson</h4>
                   <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
@@ -171,7 +171,7 @@
 
             <li class="message-item">
               <a href="#">
-                <img src="{{asset('admin/assets/img/messages-3.jpg')}}" alt="" class="rounded-circle">
+                <img src="{{asset('adminsection/assets/img/messages-3.jpg')}}" alt="" class="rounded-circle">
                 <div>
                   <h4>David Muldon</h4>
                   <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
@@ -194,7 +194,7 @@
         <li class="nav-item dropdown pe-3">
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <img src="{{asset('admin/assets/img/profile-img.jpg')}}" alt="Profile" class="rounded-circle">
+            <img src="{{asset('adminsection/assets/img/profile-img.jpg')}}" alt="Profile" class="rounded-circle">
             <span class="d-none d-md-block dropdown-toggle ps-2">M. Roshid</span>
           </a><!-- End Profile Iamge Icon -->
 
@@ -523,17 +523,17 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
 <!-- Vendor JS Files -->
-<script src="{{ asset('admin/assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
-<script src="{{ asset('admin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('admin/assets/vendor/chart.js/chart.umd.js') }}"></script>
-<script src="{{ asset('admin/assets/vendor/echarts/echarts.min.js') }}"></script>
-<script src="{{ asset('admin/assets/vendor/quill/quill.min.js') }}"></script>
-<script src="{{ asset('admin/assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
-<script src="{{ asset('admin/assets/vendor/tinymce/tinymce.min.js') }}"></script>
-<script src="{{ asset('admin/assets/vendor/php-email-form/validate.js') }}"></script>
+<script src="{{ asset('adminsection/assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
+<script src="{{ asset('adminsection/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('adminsection/assets/vendor/chart.js/chart.umd.js') }}"></script>
+<script src="{{ asset('adminsection/assets/vendor/echarts/echarts.min.js') }}"></script>
+<script src="{{ asset('adminsection/assets/vendor/quill/quill.min.js') }}"></script>
+<script src="{{ asset('adminsection/assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
+<script src="{{ asset('adminsection/assets/vendor/tinymce/tinymce.min.js') }}"></script>
+<script src="{{ asset('adminsection/assets/vendor/php-email-form/validate.js') }}"></script>
 
 <!-- Template Main JS File -->
-<script src="{{ asset('admin/assets/js/main.js') }}"></script>
+<script src="{{ asset('adminsection/assets/js/main.js') }}"></script>
 
 
 </body>
